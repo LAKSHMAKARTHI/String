2cf6d84041f25e0762cadf65a8576274cc052e40
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2016-08-26 12:15:52 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class reverse{
+public static void main(String lk[]){
+String s=new  String();
+String s1=new  String();
+Scanner sc=new Scanner(System.in);
+s=sc.next();
+S1=s.reverse();
+System.out.println(s);
+
+}
+}
